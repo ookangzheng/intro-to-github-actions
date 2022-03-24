7efafdae3ca254cd727fd0e17412268538f586c0
ookangzheng <okz5289@gmail.com>
2022-03-24 20:03:18 +0800

    fix: remove unsed var

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bye</h1>
-        <p>
-         Hi there!
-        </p>
-        
+        <h1>Bye</h1>        
       </header>
     </div>
   );
