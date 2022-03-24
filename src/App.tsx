5ca447ebce50cd8eab55611dffb2e63c165969d2
ookangzheng <okz5289@gmail.com>
2022-03-24 20:01:40 +0800

    chore: change title

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hi and Bye !!</h1>
+        <h1>Bye</h1>
         <p>
          Hi there!
         </p>
